d0885486c21b5f75f5afa9e5d655b33eefc9dfe2
Molly <mollly255@gmail.com>
2025-09-16 15:00:18 +0530

    New changes

--- 15 september/script.js
+++ 15 september/script.js
@@ -12,11 +12,36 @@
 // console.log(tag);
 
 //by class
-const abc=document.getElementsByClassName("random");
-console.log(abc);
-abc.style.backgroundColor="yellow";
+// const abc=document.getElementsByClassName("random");
+// console.log(abc);
+// abc.style.backgroundColor="yellow";
 
 //query selector
 // const q1=document.querySelector("#Box1");
 // console.log(q1);
 // q1.style.backgroundColor="pink";
+
+// const b1=document.getElementById('Box2');
+ // b1.innerHTML="<h1>Hello</h1>";
+// b1.classList.add('round-border');
+
+//add event listner
+//const abs= document.getElementById('Box3')
+
+// const boxes= document.getElementsByClassName('parent');
+// for(let i=0; i<boxes.lenght; i++){
+//     boxes[i].classList.add('round-border');
+// }
+
+let box1 = document.getElementById("Box1");
+box1.addEventListener("click", function() {
+  box1.style.backgroundColor = "yellow";
+  console.log("Box 1 clicked!");
+});
+
+
+let box3 = document.getElementById("Box3");
+box3.addEventListener("mouseover", function() {
+  box3.style.backgroundColor = "lightgreen";
+  console.log("Mouse is over Box 3!");
+});
